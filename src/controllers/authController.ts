27efbd016505fb857a8389d80108668f6b55669f
Sjabloons <kristoflafaut@gmail.com>
2025-03-03 12:20:29 +0100

    add token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -107,6 +107,7 @@ export const login = async (req: Request, res: Response) => {
         res.status(200).json({
             message: "Login successful",
             user: userObj,
+            token: token,
         });
     } catch (error) {
         console.error(error);
