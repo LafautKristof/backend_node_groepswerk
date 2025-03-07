d7e703714473bb58a93cc807c24095fe3167e1b5
Sjabloons <kristoflafaut@gmail.com>
2025-03-07 20:20:17 +0100

    30_Login_staticFolder_BE

---
+++ src/public/ts/login.ts
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById("login-form") as HTMLFormElement;
+const errorDiv = document.querySelector(".error") as HTMLDivElement;
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(loginForm);
+    const email_phone = formData.get("email_phone") as string;
+    const password = formData.get("password") as string;
+    try {
+        const response = await fetch("/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email_phone, password }),
+        });
+        const data = await response.json();
+        console.log(data);
+        if (response.ok && data.user === "admin") {
+            window.location.href = "/";
+        } else {
+            const errorDiv = document.querySelector(".error") as HTMLDivElement;
+            errorDiv.textContent = data.message;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
