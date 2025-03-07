3dd3474405b259913ce39a99b977c75648ca6f56
Sjabloons <kristoflafaut@gmail.com>
2025-03-07 16:04:56 +0100

    30_Login_staticFolder_BE

---
+++ src/controllers/adminController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { User } from "../models/UserModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env";
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email_phone, password } = req.body;
+        const user = await User.findOne({ email_phone });
+        if (!user) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return;
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return;
+        }
+        if (user.role !== "admin") {
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
+        const token = jwt.sign(
+            { _id: user._id, email_phone: user.email_phone },
+            JWT_SECRET as string,
+            { expiresIn: "1d" }
+        );
+
+        res.cookie("token", token, {
+            maxAge: 60 * 60 * 24 * 7,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: "lax",
+        });
+        res.status(200).json({ message: "Login successful" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error Login" });
+    }
+};
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { notFound } from "./controllers/notFoundController";
 import mongoose from "mongoose";
 import ProductRoutes from "./Routes/ProductRoutes";
 import AuthRoutes from "./Routes/AuthRoutes";
+import AdminRoutes from "./Routes/AdminRoutes";
 import cookieParser from "cookie-parser";
 // Variables
 const app = express();
@@ -15,10 +16,18 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("src/public"));
+
+app.get("/login", async (req, res) => {
+    res.send("login");
+});
 
 // Routes
 app.use("/api/auth", AuthRoutes);
 app.use("/api/products", ProductRoutes);
+app.use("/api/admin", AdminRoutes);
 app.all("*", notFound);
 
 // Database connection
