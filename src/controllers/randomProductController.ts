112a9446f9f05defd1e650fd84e19363c1609410
Sjabloons <kristoflafaut@gmail.com>
2025-03-13 20:35:08 +0100

    debugging3

--- src/controllers/randomProductController.ts
+++ src/controllers/randomProductController.ts
@@ -9,16 +9,24 @@ import { VideoCard } from "../models/VideoCardModel";
 export const getRandomProduct = async (req: Request, res: Response) => {
     try {
         const { count } = req.params;
-        const randomProduct = await ComputerScreen.aggregate([
-            { $unionWith: { coll: "headphones", pipeline: [] } },
-            { $unionWith: { coll: "keyboards", pipeline: [] } },
-            { $unionWith: { coll: "mice", pipeline: [] } },
-            { $unionWith: { coll: "rams", pipeline: [] } },
-            { $unionWith: { coll: "videocards", pipeline: [] } },
-            { $sample: { size: Number(count) } },
-        ]);
-        console.log(randomProduct);
-        res.status(200).json(randomProduct);
+        const ran1 = await ComputerScreen.aggregate([{ $sample: { size: 2 } }]);
+        const ran2 = await Headphone.aggregate([{ $sample: { size: 2 } }]);
+        const ran3 = await Keyboard.aggregate([{ $sample: { size: 2 } }]);
+        const ran4 = await Mouse.aggregate([{ $sample: { size: 2 } }]);
+        const ran5 = await Ram.aggregate([{ $sample: { size: 2 } }]);
+        const ran6 = await VideoCard.aggregate([{ $sample: { size: 2 } }]);
+        let randProduct = [
+            ...ran1,
+            ...ran2,
+            ...ran3,
+            ...ran4,
+            ...ran5,
+            ...ran6,
+        ];
+        console.log(randProduct);
+        randProduct = randProduct.sort(() => Math.random() - 0.5);
+        const randomProduct = randProduct.slice(0, parseInt(count));
+        res.status(200).json({ data: randomProduct });
     } catch (error) {
         console.error(error);
         res.status(500).send("getRandomProduct error");
