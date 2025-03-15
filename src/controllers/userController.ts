8df657c76d5b3a096a7a6d06aa713b5e396a2245
Sjabloons <kristoflafaut@gmail.com>
2025-03-15 16:55:47 +0100

    add response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -14,7 +14,9 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const addToCart = async (req: Request, res: Response) => {
     try {
         console.log(req.body);
+        res.status(200).json(req.body);
     } catch (error) {
         console.log(error);
+        res.status(500).send("addToCart error");
     }
 };
