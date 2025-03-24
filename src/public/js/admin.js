396fe89e69525158c8b40374eef972737b9a9343
Sjabloons <kristoflafaut@gmail.com>
2025-03-24 15:05:24 +0100

    ....

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,31 +1,26 @@
-const deleteBtns = document.querySelectorAll("#deleteBtn");
+const deleteBtns = document.querySelectorAll(".deleteBtn");
 
 deleteBtns.forEach((btn) => {
     btn.addEventListener("click", async (e) => {
         const id = e.target.dataset.id;
         const collection = e.target.dataset.collection;
-        try {
-            const response = await removeProduct(id, collection);
-            console.log(response);
-            location.reload();
-        } catch (error) {
-            console.error(error);
-        }
+
+        const response = await removeProduct(id, collection);
+        console.log(response);
+        location.reload();
+
+        console.error(error);
     });
 });
 const removeProduct = async (id, collection) => {
-    try {
-        console.log("id", id);
-        console.log("collection", collection);
-        const response = await fetch(
-            `https://backend-node-groepswerk.onrender.com/${collection}/${id}`,
-            {
-                method: "DELETE",
-            }
-        );
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
+    console.log("id", id);
+    console.log("collection", collection);
+    const response = await fetch(
+        `https://backend-node-groepswerk.onrender.com/${collection}/${id}`,
+        {
+            method: "DELETE",
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 };
