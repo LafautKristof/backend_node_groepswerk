19a72065326aa7fd3fc19b53026114a6559ddd1b
Sjabloons <kristoflafaut@gmail.com>
2025-03-24 14:57:32 +0100

    ...

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -6,25 +6,26 @@ deleteBtns.forEach((btn) => {
         const collection = e.target.dataset.collection;
         try {
             const response = await removeProduct(id, collection);
+            console.log(response);
             location.reload();
         } catch (error) {
             console.error(error);
         }
     });
-    const removeProduct = async (id, collection) => {
-        try {
-            console.log("id", id);
-            console.log("collection", collection);
-            const response = await fetch(
-                `https://backend-node-groepswerk.onrender.com/${collection}/${id}`,
-                {
-                    method: "DELETE",
-                }
-            );
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
 });
+const removeProduct = async (id, collection) => {
+    try {
+        console.log("id", id);
+        console.log("collection", collection);
+        const response = await fetch(
+            `https://backend-node-groepswerk.onrender.com/${collection}/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
